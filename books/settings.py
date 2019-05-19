a2fa8b0e643e059f4f6708e800205677e2dfa715
davilauy <50780851+davilauy@users.noreply.github.com>
2019-05-18 22:17:33 -0300

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -16,8 +16,8 @@ NEWSPIDER_MODULE = 'books.spiders'
 HTTPCACHE_ENABLED = True
 
 #CSV IMPORTACION
-ITEM_PIPELINES = {'mercado.pipelines.MercadoPipeline': 500,
-					'mercado.pipelines.MercadoImagenesPipeline': 600, }
+ITEM_PIPELINES = {'books.pipelines.MercadoPipeline': 500,
+					'books.pipelines.MercadoImagenesPipeline': 600, }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'mercado (+http://www.yourdomain.com)'
